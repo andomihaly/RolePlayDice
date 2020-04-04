ccc98019275f45d750a8e09810705e9150ca7d53
andomihaly <ando.mihaly@cleanit.hu>
2020-04-04 17:07:01 +0200

    Add d6 - dice6

---
+++ RandomDice/Dices/Dice6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RandomDice.Dices
+{
+    public class Dice6 : Dice
+    {
+        enum Dice6Value
+        {
+            PlusOne = DiceValue.PlusOne,
+            PlusTwo = DiceValue.PlusTwo,
+            PlusThree = DiceValue.PlusThree,
+        }
+        private IntervalRandomGenerator randomGenerator;
+
+        public Dice6(IntervalRandomGenerator randomGenerator)
+        {
+            this.randomGenerator = randomGenerator;
+        }
+
+        public DiceValue throwADice()
+        {
+            Dice6Value value;
+
+            int generatedNumber = randomGenerator.generateRandomNumber(1, 3);
+            if (Enum.IsDefined(typeof(Dice6Value), generatedNumber) && Enum.IsDefined(typeof(DiceValue), generatedNumber))
+            {
+                value = (Dice6Value)generatedNumber;
+            }
+            else
+                throw new InvalidDiceValueException("d6 value: " + generatedNumber);
+            return (DiceValue)value;
+        }
+    }
+}
---
+++ RandomDiceTests/Dices/Dice6Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomDice.RandomGenerator;
+using RandomDice.Dices;
+
+namespace RandomDice.Tests
+{
+    [TestClass()]
+    public class Dice6Tests
+    {
+        [TestMethod()]
+        public void throwAd3Test()
+        {
+            Dice dice = new Dice6(new VisualStudioRandomGenerator());
+            int dvalue = (int)dice.throwADice();
+            for (int i=0; i<100; i++)
+            { 
+                Assert.IsTrue(1 <= dvalue && dvalue <= 6, "value not fit for d6:" + dvalue);
+            }
+        }
+    }
+}
